93bdcaf1b445d912d6eab7e3999d3bfca23b83c2
DeLukzCoder <LukaszKrawczyk@Lukaszs-MacBook-Pro.local>
2015-12-01 11:55:53 +0100

    Lukz

--- src/Server/src/java/models/FlightModel.java
+++ src/Server/src/java/models/FlightModel.java
@@ -2,4 +2,31 @@ package models;
 
 public class FlightModel {
     
+    private int numberOfSeats;
+    private int numberOfSeatsLeft;
+    private String flightNumber;
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public int getNumberOfSeatsLeft() {
+        return numberOfSeatsLeft;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setNumberOfSeatsLeft(int numberOfSeatsLeft) {
+        this.numberOfSeatsLeft = numberOfSeatsLeft;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
 }
