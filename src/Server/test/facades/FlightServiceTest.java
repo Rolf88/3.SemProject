aaa45a03f28c69b6914904cd8450948e759d693a
Rolf88 <cb88@jubii.dk>
2015-12-01 11:52:21 +0100

    test on flightservice

--- src/Server/test/facades/FlightServiceTest.java
+++ src/Server/test/facades/FlightServiceTest.java
@@ -1,5 +1,9 @@
 package facades;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import junit.framework.Assert;
 import models.FlightModel;
@@ -33,5 +37,33 @@ public class FlightServiceTest {
         List<FlightModel> flights = flightService.findAllFlights();
 
         Assert.assertNotNull(flights);
+    }
+
+    @Test
+    public void test_findFlights_isNotNull() throws ParseException {
+        String iataOrigin = "CBH";
+        String iataDestination = "QAR";
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        Date departure = sdf1.parse("01-01-2016 06:00:00");
+
+        FlightService flightService = new FlightService();
+
+        List<FlightModel> flights = flightService.findFlights(iataOrigin, iataDestination, departure);
+
+        Assert.assertNotNull(flights);
+    }
+
+    @Test
+    public void test_findFlights_isNotEmpty() throws ParseException {
+        String iataOrigin = "CBH";
+        String iataDestination = "QAR";
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        Date departure = sdf1.parse("01-01-2016 06:00:00");
+
+        FlightService flightService = new FlightService();
+
+        List<FlightModel> flights = flightService.findFlights(iataOrigin, iataDestination, departure);
+
+        Assert.assertFalse(flights.isEmpty());
     }
 }
